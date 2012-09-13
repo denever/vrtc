f645950eb77370ec5b75f831dec52549802d29ab
Giuseppe Martino <martinogiuseppe@gmail.com>
2012-09-13 10:56:35 +0200

    Aggiunta la distizione di profilo in base al tipo di utente Aggiunta la possibilità dell'utente di scegliere se esere paziente o medico Aggiunte viste e template necessari

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,12 @@ from django.contrib.auth.decorators import login_required, permission_required
 from accounts.views import *
 
 urlpatterns = patterns('accounts.views',
-                       url(r'^profile/$',
-                           login_required(ProfileView.as_view()),
-                           name='profile'
-                       ),
-                   )
+		       url(r'^profile/$',
+			   login_required(ProfileView.as_view()),
+			   name='profile'
+		       ),
+		       url(r'^settype/(?P<type>\w)/$',
+			   login_required(UserSetTypeView.as_view()),
+			   name='user-settype'
+		       ),
+		   )
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,26 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from django.views.generic import TemplateView
+from django.views.generic import View
+
 from django.utils import simplejson
 
 from accounts.models import UserProfile
 
 class ProfileView(TemplateView):
     template_name = "profile.html"
+
+class UserSetTypeView(View):
+    def get(self, request, *args, **kwargs):
+        up = self.request.user.get_profile()
+        try:
+            up.user_type =  kwargs['type']
+            up.save()
+        except Exception, e:
+            print e
+        return redirect('profile')
+
+    def post(self, request, *args, **kwargs):
+        pass
