7505fe2f739b60edd64ade937bdc59411f692100
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-07-16 20:30:21 +0200

    reverted settings to a dev sqlite db

--- vrtc/settings.py
+++ vrtc/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-	'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-	'NAME': '/home/denever/tmp/mess/db/vrtc.db',                      # Or path to database file if using sqlite3.
-	'USER': '',                      # Not used with sqlite3.
-	'PASSWORD': '',                  # Not used with sqlite3.
-	'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-	'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/denever/tmp/mess/db/vrtc.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -135,23 +135,23 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
-	'require_debug_false': {
-	    '()': 'django.utils.log.RequireDebugFalse'
-	}
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
     },
     'handlers': {
-	'mail_admins': {
-	    'level': 'ERROR',
-	    'filters': ['require_debug_false'],
-	    'class': 'django.utils.log.AdminEmailHandler'
-	}
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
     'loggers': {
-	'django.request': {
-	    'handlers': ['mail_admins'],
-	    'level': 'ERROR',
-	    'propagate': True,
-	},
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
     }
 }
 
