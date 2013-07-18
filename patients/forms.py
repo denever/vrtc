e6ce956ceb51ed6c42a29193013aab39298296c9
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-07-18 13:09:28 +0200

    Added a cancel button and removed reverse

--- patients/forms.py
+++ patients/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 from patients.models import Patient
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Button
 from patients.widgets import BootstrapDateInput
 
 class PatientForm(forms.ModelForm):
@@ -19,6 +19,7 @@ class PatientForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_class = 'form-horizontal'
-        self.helper.form_action = reverse('patient-create')
+        self.helper.form_action = 'patient-create'
         self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Button('cancel', 'Cancel'))        
         super(PatientForm, self).__init__(*args, **kwargs)
